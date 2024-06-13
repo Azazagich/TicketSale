d25c2570aab1e9d7e9c5265ff05d04992fd726ff
Azazagich <qwertyu2289@ukr.net>
2024-06-13 12:03:09 +0300

    add: all fields and getters with setters from class AgeGroup to AgeGroupDTO

--- src/main/java/com/study/service/dto/AgeGroupDTO.java
+++ src/main/java/com/study/service/dto/AgeGroupDTO.java
@@ -1,9 +1,90 @@
 package com.study.service.dto;
 
+import com.study.domain.AgeGroup;
+import com.study.domain.Ticket;
+
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * A DTO for the {@link com.study.domain.AgeGroup} entity.
  */
 public class AgeGroupDTO implements Serializable {
+    private int id;
+    private String type;
+    private Set<TicketDTO> tickets = new HashSet<>();
+
+    public AgeGroupDTO() { }
+
+    public AgeGroupDTO(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public AgeGroupDTO type(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public AgeGroupDTO id(int id){
+        this.id = id;
+        return this;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Set<TicketDTO> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(Set<TicketDTO> tickets) {
+        this.tickets = tickets;
+    }
+
+
+    public void addTicket(TicketDTO ticket){
+        tickets.add(ticket);
+    }
+
+    public void removeTicket(TicketDTO ticket){
+        tickets.remove(ticket);
+    }
+
+    @Override
+    public String toString() {
+        return "AgeGroupDTO{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", tickets=" + tickets +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AgeGroupDTO that = (AgeGroupDTO) o;
+        return id == that.id && Objects.equals(type, that.type) && Objects.equals(tickets, that.tickets);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type, tickets);
+    }
 }
