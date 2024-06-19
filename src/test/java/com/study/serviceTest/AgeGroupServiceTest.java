d312efd8f0df13904f49a7638bb50fedf518ef36
Azazagich <qwertyu2289@ukr.net>
2024-06-19 09:55:26 +0300

    update: Test for AgeGroupService

--- src/test/java/com/study/serviceTest/AgeGroupServiceTest.java
+++ src/test/java/com/study/serviceTest/AgeGroupServiceTest.java
@@ -1,6 +1,5 @@
 package com.study.serviceTest;
 
-import com.study.domain.AgeGroup;
 import com.study.repository.AgeGroupRepository;
 import com.study.service.AgeGroupService;
 import com.study.service.dto.AgeGroupDTO;
@@ -15,9 +14,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * This class contains unit tests for the {@link AgeGroupService} class.
+ * The tests cover various operations such as finding, checking existence,
+ * updating, deleting, and saving {@link AgeGroupDTO} entities using the service layer.
  */
 public class AgeGroupServiceTest {
+
+    private final String AGE_GROUP_ADULT_TYPE = "Дорослий";
+    private final String AGE_GROUP_NAME_CHILD_TYPE = "Дитина";
+    private final String AGE_GROUP_NAME_BABIES_TYPE = "Малюки";
+    private final String AGE_GROUP_RETIREE_CHILD_TYPE = "Пенсіонер";
 
     private AgeGroupDTO ageGroupDTO1;
     private AgeGroupDTO ageGroupDTO2;
@@ -27,6 +33,10 @@ public class AgeGroupServiceTest {
     private AgeGroupMapper ageGroupMapper;
     private AgeGroupRepository ageGroupRepository;
 
+    private AgeGroupDTO createDTO(String type){
+        return new AgeGroupDTO().type(type);
+    }
+
     @BeforeEach
     void setUp() {
         ageGroupRepository = new AgeGroupRepository();
@@ -34,9 +44,9 @@ public class AgeGroupServiceTest {
 
         ageGroupService = new AgeGroupService(ageGroupRepository, ageGroupMapper);
 
-        ageGroupDTO1 = ageGroupService.save(new AgeGroupDTO().type("Інвалід"));
-        ageGroupDTO2 = ageGroupService.save(new AgeGroupDTO().type("Військовий"));
-        ageGroupDTO3 = ageGroupService.save(new AgeGroupDTO().type("Дитина"));
+        ageGroupDTO1 = ageGroupService.save(createDTO(AGE_GROUP_ADULT_TYPE));
+        ageGroupDTO2 = ageGroupService.save(createDTO(AGE_GROUP_RETIREE_CHILD_TYPE));
+        ageGroupDTO3 = ageGroupService.save(createDTO(AGE_GROUP_NAME_CHILD_TYPE));
     }
 
     @AfterEach
@@ -47,29 +57,32 @@ public class AgeGroupServiceTest {
 
     @Test
     void save() {
-        AgeGroupDTO saved = ageGroupService.save(new AgeGroupDTO().type("test"));
+        AgeGroupDTO saved = ageGroupService.save(createDTO(AGE_GROUP_ADULT_TYPE));
 
         assertTrue(ageGroupService.existById(saved.getId()));
 
         AgeGroupDTO saved2 = ageGroupService.findById(saved.getId()).orElseThrow();
 
-        assertEquals("test", saved.getType());
+        assertEquals(AGE_GROUP_ADULT_TYPE, saved.getType());
         assertEquals(saved2.getType(), saved.getType());
     }
 
-    @Test
+     @Test
     void saveAll() {
         List<AgeGroupDTO> ageGroupsDTO = new ArrayList<>();
-        ageGroupsDTO.add(ageGroupDTO1);
-        ageGroupsDTO.add(ageGroupDTO2);
+        AgeGroupDTO ageGroupDTO4 = createDTO(AGE_GROUP_NAME_BABIES_TYPE);
+        AgeGroupDTO ageGroupDTO5 = createDTO(AGE_GROUP_RETIREE_CHILD_TYPE);
+        ageGroupsDTO.add(ageGroupDTO4);
+        ageGroupsDTO.add(ageGroupDTO5);
         List<AgeGroupDTO> ageGroupDTOS = ageGroupService.saveAll(ageGroupsDTO);
         assertTrue(ageGroupService.existById(ageGroupDTOS.get(0).getId()));
         assertTrue(ageGroupService.existById(ageGroupDTOS.get(1).getId()));
+        assertEquals(ageGroupDTO4.getType(), ageGroupDTOS.get(0).getType());
+        assertEquals(ageGroupDTO5.getType(), ageGroupDTOS.get(1).getType());
     }
 
     @Test
     void findById() {
-        ageGroupService.save(ageGroupDTO1);
         assertTrue(ageGroupService.existById(ageGroupDTO1.getId()));
     }
 
@@ -78,14 +91,18 @@ public class AgeGroupServiceTest {
         assertEquals(3, ageGroupService.findAll().size());
         assertTrue(ageGroupService.existById(ageGroupDTO1.getId()));
         assertTrue(ageGroupService.existById(ageGroupDTO2.getId()));
-
+        assertTrue(ageGroupService.existById(ageGroupDTO3.getId()));
+        assertNotNull(ageGroupService.findAll());
     }
 
 
     @Test
     void updateId() {
-        assertTrue(ageGroupService.updateId(ageGroupDTO2.getId(), ageGroupDTO3));
-        assertTrue(ageGroupService.existById(ageGroupDTO3.getId()));
+        AgeGroupDTO ageGroupDTO4 = createDTO(AGE_GROUP_NAME_BABIES_TYPE);
+
+        assertTrue(ageGroupService.updateId(ageGroupDTO2.getId(), ageGroupDTO4));
+        assertTrue(ageGroupService.existById(ageGroupDTO4.getId()));
+        assertEquals(ageGroupDTO4.getType(), ageGroupService.findById(ageGroupDTO4.getId()).get().getType());
     }
 
 
@@ -98,11 +115,7 @@ public class AgeGroupServiceTest {
 
 
     @Test
-    void DeleteAll() {
-        ageGroupService.save(ageGroupDTO1);
-        ageGroupService.save(ageGroupDTO2);
-        ageGroupService.save(ageGroupDTO3);
-
+    void deleteAll() {
         List<AgeGroupDTO> ageGroupsDTODelete = List.of(ageGroupDTO1, ageGroupDTO2);
 
         assertTrue(ageGroupService.existById(ageGroupDTO1.getId()));
